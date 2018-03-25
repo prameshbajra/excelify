1e31f009f2e78f570f7acc3eff5c53c5e866153b
Suzal3579 <pe.messh@gmail.com>
2018-03-25 08:22:39 +0530

    Using firebase storage as database ... Trying things out ...

---
+++ src/js/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+    <div className="App">
+        Create react app
+        <br />
+        <h1>Boiler plate</h1>
+    </div>
+);
+
+export default App;
